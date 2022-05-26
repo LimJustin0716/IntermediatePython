dcdecdee33de9879c777afa8a59edd4effbc93b8
justinlim16 <justinlim.dn98@gmail.com>
2022-05-27 05:06:52 +0800

    Coding Exercise 4

---
+++ CodingExercise2.8.py
@@ -0,0 +1,17 @@
+groceries = {"chicken": 8, "apples": 6, "cucumbers": 3, "milk": 2, "oranges": 4}
+
+remove = groceries.pop("oranges")
+
+print(groceries)
+
+speakers = {"Sir Rafael": 54, "Ms. Joan": 33, "Miss Dana": 67}
+
+name = speakers.keys()
+print(name)
+
+tryout_result = {"Carl": "passed", "Quentin": "failed", "John Y.": "passed", "Peter": "failed"
+                 , "Ma T.": "passed", "Joseph": "passed", "Jone": "failed", "Jorge": "failed"
+                 , "George": "passed", "Ben": "passed", "Jerome": "passed", "Rick": "failed"
+                 , "Max G.": "failed", "John P.": "failed", "Vince": "passed"}
+
+print(tryout_result.get("Jorge"))
