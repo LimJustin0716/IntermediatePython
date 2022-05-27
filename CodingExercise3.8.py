04ca70a6d037a3a0d295f47c9c4d5905c62364ce
justinlim16 <justinlim.dn98@gmail.com>
2022-05-28 00:28:59 +0800

    Coding Exercise 6

---
+++ CodingExercise3.8.py
@@ -0,0 +1,10 @@
+#f = open("pythonessay.txt", "r")
+#print(f.read())
+#print(f.readline())
+#for x in f:
+#    print(x)
+import os
+if os.path.exists("pythonessay.txt"):
+    os.remove("pythonessay.txt")
+else:
+    print("file does not exist")
